0d73656fc523af6ae40ce481aeb35a4e929625b8
Adam Lyon <lyon@fnal.gov>
2017-04-09 23:30:13 -0500

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Installations for ubuntu
+export DEBIAN_FRONTEND=noninteractive
+
+# Update ubuntu
+apt-get -y update
+apt-get -y upgrade 
+apt-get -y dist-upgrade
+
+# Install kerberos and VOMS
+apt-get install -yq krb5-user
+wget http://computing.fnal.gov/authentication/krb5conf/Linux/krb5.conf -O /etc/krb5.conf
+
+# Install CVMFS (see https://cernvm.cern.ch/portal/filesystem/downloads )
+wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
+dpkg -i cvmfs-release-latest_all.deb
+rm -f cvmfs-release-latest_all.deb
+apt-get -y update
+apt-get install -y cvmfs
+
+# Fix autofs
+sed -i '/cvmfs/d' /etc/auto.master
+service autofs reload
+service autofs restart
+
+# Write domain.local
+cat > /etc/cvmfs/default.local << NNNN
+CVMFS_REPOSITORIES="\`echo \$(ls /cvmfs | grep  '\.')|tr ' ' ,\`"
+CVMFS_HTTP_PROXY=DIRECT
+NNNN
+
+# Write opensciencegrid.org.local
+cat > /etc/cvmfs/domain.d/opensciencegrid.org.local << NNNN
+CVMFS_SERVER_URL="http://cvmfs.fnal.gov:8000/cvmfs/@org@.opensciencegrid.org;http://oasis-replica.opensciencegrid.org:8000/cvmfs/@org@.opensciencegrid.org"
+CVMFS_KEYS_DIR=/etc/cvmfs/keys/opensciencegrid.org
+CVMFS_USE_GEOAPI=yes
+NNNN
+
+cvmfs_config reload
+
+# Write cvmfs_mount
+cat > /usr/local/bin/cvmfs_mount << NNNN
+sudo umount -f /cvmfs/\$1.opensciencegrid.org
+sudo mkdir -p /cvmfs/\$1.opensciencegrid.org
+sudo mount -t cvmfs \$1.opensciencegrid.org /cvmfs/\$1.opensciencegrid.org
+NNNN
+chmod a+x /usr/local/bin/cvmfs_mount
+
+# Install Singularity (see http://singularity.lbl.gov/install-linux )
+VERSION=2.2.1
+wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
+tar xvf singularity-$VERSION.tar.gz
+cd singularity-$VERSION
+./configure --prefix=/usr/local
+make
+sudo make install
+
+apt-get clean
+
+# DONE!!
